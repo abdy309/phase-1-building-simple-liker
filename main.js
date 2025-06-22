9bea7ab3ef3259710beb492210ac6f3545395229
abdul hamid <abdulhamidali903@gmail.com>
2025-06-22 17:34:57 +0300

    simple liker

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+// Hide error modal on initial load
+const errorModal = document.getElementById("modal");
+errorModal.classList.add("hidden");
 
+// Select all heart icons
+const hearts = document.querySelectorAll(".like-glyph");
 
+hearts.forEach(heart => {
+  heart.addEventListener("click", () => {
+    // Simulate server call
+    mimicServerCall()
+      .then(() => {
+        // On success, toggle the heart state
+        if (heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART;
+          heart.classList.add("activated-heart");
+        } else {
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove("activated-heart");
+        }
+      })
+      .catch((error) => {
+        // On failure, show error modal
+        errorModal.classList.remove("hidden");
+        document.getElementById("modal-message").innerText = error;
 
+        // Hide modal after 3 seconds
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+});
 
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+// This function mocks a server call
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
